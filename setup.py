e339aa0bb63599c6d29648dcadb98e4063371a5d
Gunhoon Lee <gunhoon@gmail.com>
2022-08-19 18:30:26 +0900

    Use the modern package setup
    setup.py is optional and required for editable installs
    
    For more information, see below:
    https://github.com/pypa/packaging.python.org/blob/7173541b8c0141bcc4172ca06f9729d5a7d04ba7/source/tutorials/packaging-projects.rst

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="example-pkg-gunhoon",
-    version="0.0.2",
-    author="Gunhoon Lee",
-    author_email="gunhoon@gmail.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/gunhoon/packaging-tutorial",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
+setuptools.setup()
